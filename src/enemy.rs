506d0dd9132ca40660603712c56877df70d4eb2b
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-11-14 15:58:30 +0100

    make most not pathfind and just be physics driven

--- src/enemy.rs
+++ src/enemy.rs
@@ -2,7 +2,7 @@ use std::{collections::VecDeque, iter::Map};
 
 use crate::{
     assets::{Assets, World},
-    player::Player,
+    player::{Player, update_physicsbody},
     utils::*,
 };
 use macroquad::prelude::*;
@@ -10,6 +10,8 @@ use macroquad::prelude::*;
 pub struct EnemyType {
     pub draw_fn: &'static dyn Fn(&Assets, &Enemy),
     pub speed: f32,
+    pub health: f32,
+    pub pathfind: bool,
 }
 
 pub const GREENO: EnemyType = EnemyType {
@@ -25,7 +27,9 @@ pub const GREENO: EnemyType = EnemyType {
             },
         );
     },
+    health: 20.0,
     speed: 50.0,
+    pathfind: false,
 };
 
 pub struct Enemy {
@@ -36,26 +40,29 @@ pub struct Enemy {
     pub moving_left: bool,
     pub path: Option<VecDeque<(i16, i16)>>,
     pub time_til_pathfind: f32,
+    pub velocity: Vec2,
 }
 impl Enemy {
     pub fn new(ty: &'static EnemyType, pos: Vec2) -> Self {
         Self {
             ty,
             pos,
-            health: 20.0,
+            health: ty.health,
             animation_time: 0.0,
             moving_left: false,
             path: None,
             time_til_pathfind: 0.0,
+            velocity: Vec2::ZERO,
         }
     }
     pub fn update(&mut self, delta_time: f32, player: &mut Player, world: &World) {
         let delta = player.pos - self.pos;
+        let mut target = player.pos;
         if delta.length() > 0.0 {
             self.animation_time += delta_time;
             self.time_til_pathfind -= delta_time;
 
-            if self.path.is_none() || self.time_til_pathfind <= 0.0 {
+            if self.ty.pathfind && (self.path.is_none() || self.time_til_pathfind <= 0.0) {
                 self.time_til_pathfind = 2.0;
                 self.path = world
                     .pathfind(self.pos, player.pos + 8.0)
@@ -68,10 +75,14 @@ impl Enemy {
                 if next.distance(self.pos) < 4.0 {
                     path.pop_front();
                 }
-                self.moving_left = (next - self.pos).x > 0.0;
-
-                self.pos += (next - self.pos).normalize() * delta_time * self.ty.speed;
+                target = next;
             }
+        }
+        if target.distance(self.pos) > 0.0 {
+            self.moving_left = (target - self.pos).x > 0.0;
+            self.velocity = (target - self.pos).normalize() * self.ty.speed;
+            self.pos =
+                update_physicsbody(self.pos, &mut self.velocity, delta_time, &world.collision);
         }
     }
     pub fn draw(&self, assets: &Assets) {
